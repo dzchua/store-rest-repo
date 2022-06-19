309b391e7a3e8f8d4bace0e7ea650152e5c26d17
dzchua <104882567+dzchua@users.noreply.github.com>
2022-06-19 14:31:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os #read the virtual environment
+
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
@@ -8,14 +10,21 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+#app.config['PROPAGATE_EXCEPTIONS'] = True
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') #read the first variable for cloud hosting heroku postgre, default: 2nd value for local environment.
+uri = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to know object changed but not saved in database: turn it off as it is a tracker
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request #create database
-def create_tables():
-    db.create_all()
+# @app.before_first_request #create database
+# def create_tables():
+#     db.create_all()
 
 #app.config['JWT_AUTH_URL_RULE'] = '/login' # changes /auth to /login
 jwt = JWT(app, authenticate, identity) # /auth - default
