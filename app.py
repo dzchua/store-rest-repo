cc5e1840b3c313b53da9349760df8417f3079297
unknown <dzcty11@gmail.com>
2022-06-22 12:38:40 +0800

    Update mysql

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 # if uri.startswith("postgres://"):
 #     uri = uri.replace("postgres://", "postgresql://", 1)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('CLEARDB_DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dbuser:dbpassword@127.0.0.1:3306/dbname'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to know object changed but not saved in database: turn it off as it is a tracker
 app.secret_key = 'jose'
 api = Api(app)
