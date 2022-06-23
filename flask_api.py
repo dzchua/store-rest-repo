fd2de80f5ddfe568360d58a313dba0339adac7cd
dzchua <104882567+dzchua@users.noreply.github.com>
2022-06-23 15:00:48 +0800

    Delete flask_api.py

--- flask_api.py
+++
@@ -1,5 +0,0 @@
-import mysql.connector
-
-cnx = mysql.connector.connect(user='dbuser', password='',
-                              host='localhost',
-                              database='flask_api')
